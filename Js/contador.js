889b5adddfca0405dc31d107ddfa799f306056e6
Aleinte15 <jaaleinte15@gmail.com>
2023-05-10 16:42:55 -0600

    se agregan validación con colores para dismunir o incrementar

--- Js/contador.js
+++ Js/contador.js
@@ -7,22 +7,29 @@ function sumar() {
     var contador = obtenerContador();
     var nuevoContador = contador + 1;
     document.getElementById("idContador").textContent = nuevoContador;
+    if (nuevoContador > 0) {
+        document.getElementById("idContador").style.color = "green";
+    } else if (nuevoContador == 0) {
+        document.getElementById("idContador").style.color = "black";
+    }
 }
 
 function restar() {
     var contador = obtenerContador();
-    if (contador > 0) {
-        var nuevoContador = contador - 1;
-        document.getElementById("idContador").textContent = nuevoContador;
-    } else {
-        alert("El contador es 0.");
+    var nuevoContador = contador - 1;
+    document.getElementById("idContador").textContent = nuevoContador;
+    if (nuevoContador < 0) {
+        document.getElementById("idContador").style.color = "red";
+    } else if (nuevoContador == 0) {
+        document.getElementById("idContador").style.color = "black";
     }
 }
 
 function reiniciar() {
     var contador = obtenerContador();
-    if (contador > 0) {
+    if (contador != 0) {
         document.getElementById("idContador").textContent = "0";
         alert("El contador fue reiniciado a 0.");
+        document.getElementById("idContador").style.color = "black";
     }
 }
